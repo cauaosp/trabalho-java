f30656698b41cc617bf210d09f5d9519fd739b47
cauaosp <cauaosp20@gmail.com>
2025-03-05 17:57:06 -0300

    feat: lendo o arquivo de leitura e salvando em hashmap a palavra e as linhas em que aparece

--- Program.java
+++ Program.java
@@ -3,27 +3,36 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 
 public class Program {
+
   public static void main(String[] args) {
-    String path = "C:\\Users\\cauao\\source\\repos\\estrutura_de_dados\\trabalho1\\arquivo_de_leitura.txt";
-    List<String> stringList = new ArrayList<>();
-    File arquivo_de_leitura = new File(path);
+    String caminho_arquivo_de_leitura = "C:\\Users\\cauao\\source\\repos\\estrutura_de_dados\\trabalho1\\trabalho-java\\arquivo_de_leitura.txt";
+    File arquivo_de_leitura = new File(caminho_arquivo_de_leitura);
+    HashMap<String, List<Integer>> texto_em_hash = new HashMap<>();
+    int linha = 0;
 
     try (BufferedReader br = new BufferedReader(new FileReader(arquivo_de_leitura))) {
       String line = br.readLine();
 
       while (line != null) {
-        String[] field = line.split("\\s+"); // Divide a linha em palavras
-        stringList.addAll(Arrays.asList(field)); // Adiciona as palavras à lista
-        System.out.println(Arrays.toString(field)); // Exibe as palavras
-        line = br.readLine(); // Lê a próxima linha
+        String[] field = line.split("\\s+");
+        for (String word : field) {
+          texto_em_hash.putIfAbsent(word, new ArrayList<>());
+          texto_em_hash.get(word).add(linha);
+        }
+        line = br.readLine();
+        linha++;
       }
-    } catch (IOException e) { // Captura exceções de leitura de arquivo
+
+      System.out.println(texto_em_hash);
+      System.out.println(texto_em_hash.get("de"));
+    } catch (IOException e) {
       System.out.println("An error occurred.");
       e.printStackTrace();
     }
+
   }
 }
